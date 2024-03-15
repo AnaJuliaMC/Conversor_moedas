3670bf7e040efe21e1ddb0da08ed5b00a88c3bea
Ana Julia Menegasso <130059630+AnaJuliaMC@users.noreply.github.com>
2024-03-15 08:46:57 -0300

    java

---
+++ a.js
@@ -0,0 +1,5 @@
+function converter(){
+    let origem = document.getElementById('origem').value;
+    let destino = document.getElementById('destino').value;
+    let quant = document.getElementById
+}
